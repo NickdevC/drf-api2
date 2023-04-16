23a5653901ffa1743dce4c3f8cd0a3b8f536de5f
NickdevC <nick_coles@msn.com>
2023-04-16 07:02:57 +0000

    Add post method to PostList view and include permission through django rest

--- posts/serializers.py
+++ posts/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Post
 
 
-class PostsSerializer(serializers.ModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
@@ -31,6 +31,6 @@ class PostsSerializer(serializers.ModelSerializer):
         model = Post
         fields = [
             'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
-            'created_at', 'udpated_at', 'title', 'content', 'image',
+            'created_at', 'updated_at', 'title', 'content', 'image',
             'image_filter'
         ]
